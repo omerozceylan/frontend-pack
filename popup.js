59b2ee0383ebe0bcc5f3b2d52893cdea4502e40c
Omer Ozceylan <omerozceylan40@gmail.com>
2023-03-03 06:46:00 +0300

    ++

---
+++ popup.js
@@ -0,0 +1,21 @@
+let infoButton = document.querySelectorAll('.items-info-button')
+let popUp = document.querySelector('.modal')
+let popUpCloseButton = document.querySelector('.pop-up-button')
+
+
+infoButton.forEach((item)=> {
+    item.addEventListener('click',()=>{
+        popUp.showModal()
+    })        
+})
+
+popUpCloseButton.addEventListener('click', ()=> {
+    popUp.close()
+})
+
+
+
+function openPopUp(text){
+   
+}
+
