3c65432a8ff5cbac4c930ff04862ba2ea4d087f0
Omer Ozceylan <omerozceylan40@gmail.com>
2023-03-07 23:53:32 +0300

    ^^^

--- data.js
+++ data.js
@@ -343,7 +343,7 @@ packages.forEach((item)=> {
                 </div>
                 <div class="items-button-container">
                     <div class="items-title">${item.name}</div>
-                    <img src="src/delete.svg" class="items-info-button"/>
+                    <img src="src/info2.svg" class="items-info-button"/>
                 </div>
             </div>
   `
